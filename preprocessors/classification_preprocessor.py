7f9612a0258bc8d59afb495e924862b432093473
ririverce <ririverce@gmail.com>
2020-01-24 21:16:38 +0900

    add data augumentation function

--- preprocessors/classification_preprocessor.py
+++ preprocessors/classification_preprocessor.py
@@ -15,22 +15,34 @@ class ClassificationPreprocessor:
 
     def __random_crop_and_resize(self, image):
         image_height, image_width = image.shape[:2]
-        crop_x_min = image_width * random.random() * 0.2
-        crop_y_min = image_height * random.random() * 0.2
-        crop_x_max = image_width * (random.random() * 0.2 + 0.8)
-        crop_y_max = image_height * (random.random() * 0.2 + 0.8)
+        crop_x_min = image_width * random.random() * 0.1
+        crop_y_min = image_height * random.random() * 0.1
+        crop_x_max = image_width * (random.random() * 0.1 + 0.9)
+        crop_y_max = image_height * (random.random() * 0.1 + 0.9)
         image = image[int(crop_y_min):int(crop_y_max),
                       int(crop_x_min):int(crop_x_max)]
         image = cv2.resize(image, self.output_size)
         return image
 
-    def __random_mask(self, image, max_size_ratio=0.2):
+    def __random_rotate(self, image):
+        image = 255 - image
         image_height, image_width = image.shape[:2]
-        crop_x_min = image_width * (1 - max_size_ratio) * random.random()
-        crop_y_min = image_height * (1 - max_size_ratio) * random.random()
-        crop_x_max = image_width * max_size_ratio * random.random() + crop_x_min
-        crop_y_max = image_height * max_size_ratio * random.random() + crop_y_min
-        image[int(crop_y_min):int(crop_y_max), int(crop_x_min):int(crop_x_max)] = 255
+        center = (int(image_width/2), int(image_width/2))
+        angle = random.random() * 360
+        matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+        image = cv2.warpAffine(image, matrix, (image_width, image_height))
+        image = 255 - image
+        return image        
+
+    def __random_cutout(self, image, size_ratio=0.2, num_cut=3):
+        image_height, image_width = image.shape[:2]
+        for i in range(num_cut):
+            crop_x_min = image_width * (1 - size_ratio) * random.random()
+            crop_y_min = image_height * (1 - size_ratio) * random.random()
+            crop_x_max = image_width * size_ratio + crop_x_min
+            crop_y_max = image_height * size_ratio + crop_y_min
+            color = random.random() * 255
+            image[int(crop_y_min):int(crop_y_max), int(crop_x_min):int(crop_x_max)] = int(color)
         return image
 
     def __random_brightness(self, image, range=(0.8, 1.2)):
@@ -38,6 +50,12 @@ class ClassificationPreprocessor:
         image = image * scale
         image = np.clip(image, 0, 255)
         image = image.astype(np.uint8)
+        return image
+
+    def __add_gaussian_nois(self, image, mean=0, var=10):
+        gauss = np.random.normal(mean, var, image.shape)
+        image = image + gauss
+        image = np.clip(image, 0, 255).astype(np.uint8)
         return image
 
     def run(self, queue):
@@ -54,10 +72,10 @@ class ClassificationPreprocessor:
                      data["vowel_diacritic"],
                      data["consonant_diacritic"]]
             image = self.__random_crop_and_resize(image)
-            image = self.__random_mask(image)
-            image = self.__random_mask(image)
-            image = self.__random_mask(image)
+            image = self.__random_rotate(image)
+            image = self.__random_cutout(image)
             image = self.__random_brightness(image)
+            image = self.__add_gaussian_nois(image)
             #cv2.imshow("test", image)
             #cv2.waitKey(0)
             image = np.transpose(image, (2, 0, 1))
@@ -77,6 +95,7 @@ class ClassificationPreprocessor:
 
 
 def test():
+    import os
     import sys
     sys.path.append("../")
     import multiprocessing
